ddac9cd6e9207a70f90a5987d44750c7e0448883
gongdo <gongdo@live.com>
2017-11-10 15:50:04 +0900

    remove artifacts

--- CSharp/src/Pipelines/Squaring.cs
+++ CSharp/src/Pipelines/Squaring.cs
@@ -4,10 +4,17 @@ using System.Reactive.Linq;
 
 namespace Pipelines
 {
+    // There's no exact match for Go's chan in C#.
+    // In this particular example, Observable might be the most comparable functionality.
     public class Squaring
     {
-        public IObservable<int> Generate(params int[] numbers) => numbers.ToObservable();
+        // actually, c# doesn't need a function like this
+        // since System.Reactive already support Array<T>.ToObservable() extension method.
+        public IObservable<int> Generate(params int[] numbers) 
+            => numbers.ToObservable();
 
+        // you can say this also as pipeline, map, transformation.
+        // in C# world, 'Select' is more proper term.
         public IObservable<int> Squre(IObservable<int> input)
         {
             return input.Select(n => n * n);
